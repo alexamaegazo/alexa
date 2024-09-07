8e174de1ac4bd603cba0fac6732ffbb76e1c83bf
alexamaegazo <alexamaegazo5@gmail.com>
2024-09-08 06:15:56 +0800

    initial commit

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 console.log('Hello, World!');
-let name = "Alexa Mae Gazo";
+let name = "Alexa Mae Gazo";``
 console.log('My name is' + name);
 
 // Variable
 console.log("Variables");
 let name_first = "Cassandra";
 
+let island = 7641;
+console.log(island);
+
+// Numbers
+let a = 25;
+let b = 18;
+let sum = a + b;
+console.log(sum);
+
+//Boolean
+let isAge = Boolean('25');
+let isNum = Boolean('');
+console.log(isAge);
+console.log(isNum);
+
+//Comparison operator
+let w = 39;
+let u = 20;
+
+//Use comparison operator and store the reuslts in varaiables 
+let isWGreaterThanU = x > y;
+let isWLessThanU = x > y;
+let isWEqualToU = x === y;
+let isWNotEqualtoU = x !== y;
+
+console.log("Is W greater than U?");
+console.log("Is W less than U?");
+console.log("Is W equal to U?");
+console.log("Is W is not equal to U?");
